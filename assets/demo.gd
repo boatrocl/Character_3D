b1a61e6e01fff1d696b35fdcada579ac082e30c0
boatrocl <sorawittamanunt@gmail.com>
2025-09-21 20:24:03 +0700

    send

---
+++ assets/demo.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Blockman_noob/AnimationPlayer.play("MeleeLib/Die1")
+	$Blockman_noob2/AnimationPlayer.play("MeleeLib/Guarding")
+	$Blockman_noob3/AnimationPlayer.play("MeleeLib/root-LightRunning")
+	$Blockman_noob4/AnimationPlayer.play("MeleeLib/root-Sprint")
+	$Robot/AnimationPlayer.play("MeleeLib/HurtIdle")
+	$Robot2/AnimationPlayer.play("MeleeLib/HurtIdle")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
